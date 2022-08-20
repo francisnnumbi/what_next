3eb82cd7232b6d68784caedc9e7d5dad7457f018
Francis Nduba Numbi <francisnnumbi@yandex.com>
2022-08-20 20:28:45 +0200

    update

--- lib/modules/flexing/flexing.dart
+++ lib/modules/flexing/flexing.dart
@@ -5,86 +5,105 @@ class Flexing extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Axis axis = MediaQuery.of(context).size.width < 800
+        ? Axis.vertical
+        : Axis.horizontal;
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: const Text("Flex : Column or Row"),
       ),
       body: Center(
-        child: Column(
+        child: Flex(
+          direction: axis,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                primary: Colors.orange,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              ),
-              child: const Text(
-                "Orange",
-                style: TextStyle(
-                  fontSize: 20,
+            SizedBox(
+              width: 150,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.orange,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                ),
+                child: const Text(
+                  "Orange",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
                 ),
               ),
             ),
             const SizedBox(width: 10, height: 10),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              ),
-              child: const Text(
-                "Green",
-                style: TextStyle(
-                  fontSize: 20,
+            SizedBox(
+              width: 150,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                ),
+                child: const Text(
+                  "Green",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
                 ),
               ),
             ),
             const SizedBox(width: 10, height: 10),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blue,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              ),
-              child: const Text(
-                "Blue",
-                style: TextStyle(
-                  fontSize: 20,
+            SizedBox(
+              width: 150,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                ),
+                child: const Text(
+                  "Blue",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
                 ),
               ),
             ),
             const SizedBox(width: 10, height: 10),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                primary: Colors.red,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              ),
-              child: const Text(
-                "Red",
-                style: TextStyle(
-                  fontSize: 20,
+            SizedBox(
+              width: 150,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                ),
+                child: const Text(
+                  "Red",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
                 ),
               ),
             ),
             const SizedBox(width: 10, height: 10),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                primary: Colors.teal,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              ),
-              child: const Text(
-                "Teal",
-                style: TextStyle(
-                  fontSize: 20,
+            SizedBox(
+              width: 150,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.teal,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                ),
+                child: const Text(
+                  "Teal",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
                 ),
               ),
             ),
