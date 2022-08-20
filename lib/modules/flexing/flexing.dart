859f590a3ec386f4ae7a0482bd9c677cdde12e4d
Francis Nduba Numbi <francisnnumbi@yandex.com>
2022-08-20 20:03:20 +0200

    using flex instead of column or row

--- lib/modules/flexing/flexing.dart
+++ lib/modules/flexing/flexing.dart
@@ -8,62 +8,87 @@ class Flexing extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: const Text("Flexing"),
+        title: const Text("Flex : Column or Row"),
       ),
       body: Center(
-        child: Row(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
               onPressed: () {},
               style: ElevatedButton.styleFrom(
                 primary: Colors.orange,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
               ),
               child: const Text(
                 "Orange",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
             ),
-            const SizedBox(width: 10),
+            const SizedBox(width: 10, height: 10),
             ElevatedButton(
               onPressed: () {},
               style: ElevatedButton.styleFrom(
                 primary: Colors.green,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
               ),
               child: const Text(
                 "Green",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
             ),
-            const SizedBox(width: 10),
+            const SizedBox(width: 10, height: 10),
             ElevatedButton(
               onPressed: () {},
               style: ElevatedButton.styleFrom(
                 primary: Colors.blue,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
               ),
               child: const Text(
                 "Blue",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
             ),
-            const SizedBox(width: 10),
+            const SizedBox(width: 10, height: 10),
             ElevatedButton(
               onPressed: () {},
               style: ElevatedButton.styleFrom(
                 primary: Colors.red,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
               ),
               child: const Text(
                 "Red",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
             ),
-            const SizedBox(width: 10),
+            const SizedBox(width: 10, height: 10),
             ElevatedButton(
               onPressed: () {},
               style: ElevatedButton.styleFrom(
                 primary: Colors.teal,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
               ),
               child: const Text(
                 "Teal",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
             ),
-            const SizedBox(width: 10),
+            const SizedBox(width: 10, height: 10),
           ],
         ),
       ),
